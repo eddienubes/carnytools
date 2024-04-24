835f85a1900cd85de1f44992a8efbaae439846e0
eddienubes <cool.gegeg@gmail.com>
2024-04-24 21:02:58 +0200

    feat: some new eslint rules

--- packages/styles/src/eslint/base.js
+++ packages/styles/src/eslint/base.js
@@ -10,6 +10,7 @@ module.exports = {
         }
       }
     ],
-    'import/no-named-as-default-member': 'off'
+    'import/no-named-as-default-member': 'off',
+    'no-unused-vars': 'warn'
   }
 };
